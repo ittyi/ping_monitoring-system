452d658203dd1081ebb16a08dac0c1290a35f8f5
ittyi <vioiv.k4@gmail.com>
2021-12-18 10:20:16 +0900

    Question1にコメント追加。改善したいところ有

--- Question1/server_failure_detection.py
+++ Question1/server_failure_detection.py
@@ -1,18 +1,14 @@
 # 故障状態のサーバアドレスとそのサーバの故障期間を出力するプログラムを作成
 # 
-# まず、ログファイルを”読み込める”ようにする。
-# ファイルをパラメータとして。。という文言はないため、何とかして別フォルダにいるファイルを読み込む必要がある。
-# 
 # pingがタイムアウトした場合を故障とみなし、最初にタイムアウトしたときから、
 # 次にpingの応答が返るまでを故障期間とする。
 #
-# 
+# 実行：python server_failure_detection.py
 # #
 import datetime
 
-
-f = open('../test/test2.txt', 'r', encoding='UTF-8')
-# print(f)
+# テスト時、ここtestx.txtのxを1～4に変更すると、用意したテストで実行できる
+f = open('../test/test1.txt', 'r', encoding='UTF-8')
 
 count = 0
 fault_condition = False
@@ -20,18 +16,13 @@ time_count_start = []
 time_count_end = []
 fail_server_address = []
 
+# 本処理
 datalist = f.readlines()
 for data in datalist:
-    print(data)
-    # print(count)
-
     datasplit = data.split(",")
-    print(datasplit)
     index = len(datasplit)
-    print(datasplit[index-1])
 
     if datasplit[index-1] == ("-\n" or "-"):
-        print("check")
         count += 1
         fault_condition = True
         time_count_start.append(
@@ -47,7 +38,6 @@ for data in datalist:
         fail_server_address.append(datasplit[1])
     
     if (fault_condition == True) and datasplit[index-1] != ("-\n" or "-"):
-        print("check2")
         fault_condition = False
         time_count_end.append(
             datetime.datetime(
@@ -60,25 +50,16 @@ for data in datalist:
                 )
         )
 
-print("count:", count, len(time_count_start))
-print(fault_condition)
-print(time_count_start)
-print(time_count_end)
-
 
 # 最後まで「-」で終わってしまった場合の処理 if
-# 最後まで「-」が一つもなかった elif
-# 正常 else
+# 最後まで「-」が一つもなく正常 elif
+# 「-」が一つ以上あり、応答が返ってきた場合
 if fault_condition == True:
     print("故障中のサーバIPv4アドレス:", fail_server_address[0])
     print("故障期間: 継続中")
 elif time_count_start == []:
     print("故障したサーバIPv4アドレス: なし")
     print("故障期間: なし")
-elif len(time_count_start) == 1:
-    print("check3")
-    print("故障したサーバIPv4アドレス:", fail_server_address[0])
-    print("故障期間:", time_count_end[0] - time_count_start[0])
 else :
     for i in range(count):
         print("故障したサーバIPv4アドレス:", fail_server_address[i])
